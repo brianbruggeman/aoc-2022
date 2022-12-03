bf43e8a63325f797640ef310edfbadb5012109d8
Brian Bruggeman <1350114+brianbruggeman@users.noreply.github.com>
2022-12-02 21:04:09 -0600

    Better than part1 and part2

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -2,6 +2,6 @@ mod calories;
 
 pub fn run() {
     println!("Day 1:");
-    println!("    Part 1: {} calories", calories::calculate(None, 1));
-    println!("    Part 2: {} calories", calories::calculate(None, 3));
+    println!("    Maximum: {} calories", calories::calculate(None, 1));
+    println!("      Max 3: {} calories", calories::calculate(None, 3));
 }
