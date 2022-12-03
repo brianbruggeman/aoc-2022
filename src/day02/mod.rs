ed1cd9f70500fcb7407d4d0a1637f9e3fa5545d2
Brian Bruggeman <1350114+brianbruggeman@users.noreply.github.com>
2022-12-02 20:59:47 -0600

    Updated to see cache working

--- src/day02/mod.rs
+++ src/day02/mod.rs
@@ -6,6 +6,6 @@ pub mod rock_paper;
 pub fn run() {
     let moves = include_str!("input1.txt");
     println!("Day 2:");
-    println!("    Part 1: score {}", rock_paper::play_round_1(moves));
-    println!("    Part 2: score {}", rock_paper::play_round_2(moves));
+    println!("    Guessed: score {}", rock_paper::play_round_1(moves));
+    println!("    Actual: score {}", rock_paper::play_round_2(moves));
 }
